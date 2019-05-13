b4b93e1039d6ce5cc692f79db1fe94d67e89ebb2
Gourc Mathis <gourcmathi@eisti.eu>
2019-05-13 11:51:45 +0200

    squelette mis à jour

--- inscription.php
+++ inscription.php
@@ -2,9 +2,20 @@
 <html>
 <head lang="fr">
     <meta charset="UTF-8">
-    <title>Site</title>
-    
+    <title>EISTI - BOOK </title>
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+          rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css"
+          rel="stylesheet" type="text/css"> -->
+           <link href="./inscription.css"
+          rel="stylesheet" type="text/css">
 </head>
+
+
+
 <body>
 
 <?php
@@ -12,42 +23,18 @@ require("util.php");
 
 //Lors de la soumission des données du formulaire, insérer le nouvel abonné
 if (isset($_POST['nom']) && isset($_POST['prenom'])) {
-$tab=array('nom' => $_POST['nom'],'prenom'  => $_POST['tel'],'email' => $_POST['email'],'password' => $_POST['password']);
+$tab=array('nom' => $_POST['nom'],'prenom'  => $_POST['prenom'],'ddn' => $_POST['ddn'],'email' => $_POST['email'],'promo' => $_POST['promo'],'password' => $_POST['password']);
     insertEleve($tab);
 }
 
 
 ?>
 
-<div class="section">
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-12">
-                <ul class="nav nav-pills">
-                    <li class="active">
-                        <a href="inscription.php">Inscription</a>
-                    </li>
-                    <li>
-                        <a href="login.php">Me Connecter</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</div>
+<h1 class="titre"> EISTI - BOOK </h1>
 
 
-<div class="section">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h1 class="text-center">Inscription</h1>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="section">
+<h2 class="soustitre"> Formulaire d'inscription </h2>
+<div class="section2">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -57,7 +44,7 @@ $tab=array('nom' => $_POST['nom'],'prenom'  => $_POST['tel'],'email' => $_POST['
                             <label for="nom" class="control-label">Nom</label>
                         </div>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="nom" name="nom" placeholder="Nom">
+                            <input type="text" class="form-control" id="nom" name="nom" placeholder="Dupont">
                         </div>
                     </div>
                     <div class="form-group">
@@ -65,28 +52,54 @@ $tab=array('nom' => $_POST['nom'],'prenom'  => $_POST['tel'],'email' => $_POST['
                             <label for="prenom" class="control-label">Prénom</label>
                         </div>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Prénom">
+                            <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Jean">
                         </div>
                     </div>
-                    <div class="form-group">
+
+                     <div class="form-group">
                         <div class="col-sm-2">
-                            <label for="email" class="control-label">Email</label>
+                            <label for="ddn" class="control-label">Date de naissance</label>
                         </div>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            <input type="date" class="form-control" id="ddn" name="ddn" placeholder="02/07/1998">
                         </div>
                     </div>
-                    <div class="form-group">
+
+                     <div class="form-group">
                         <div class="col-sm-2">
-                            <label for="password" class="control-label">Mot de passe</label>
+                            <label for="email" class="control-label">Adresse email </label>
                         </div>
                         <div class="col-sm-10">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="bernedohug@eisti.eu">
                         </div>
                     </div>
+
+                     <div class="form-group">
+                        <div class="col-sm-2">
+                            <label for="promo" class="control-label">Année de promotion</label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" id="promo" name="promo" placeholder="2022">
+                        </div>
+                    </div>
+
+                     <div class="form-group">
+                        <div class="col-sm-2">
+                            <label for="password" class="control-label">Mot de passe </label>
+                        </div>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="coucoujesuislemotdepasse">
+                        </div>
+                    </div>
+                  
+
+
+
+
+
                     <div class="form-group">
                         <div class="col-sm-offset-2 col-sm-10">
-                            <button type="submit" class="btn btn-default">Créer mon compte</button>
+                            <button type="submit" class="bouton">M'inscrire</button>
                         </div>
                     </div>
                 </form>
@@ -94,6 +107,39 @@ $tab=array('nom' => $_POST['nom'],'prenom'  => $_POST['tel'],'email' => $_POST['
         </div>
     </div>
 </div>
+
+
+
+
+
+<div class="section3">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <ul >
+                   
+                    <li class="menuli" >
+                        <a class="nonlien" href="inscription.php">Inscription</a>
+                    </li>
+                    <li class="menuli">
+                        <a class="nonlien" href="login.php">Me Connecter</a>
+                    </li>
+
+                    <li class="menuli">
+                        <a class="nonlien" href="login.php?action=logout">Me déconnecter</a>
+                    </li>
+
+                    <li  class="menuli">
+                        <a class="nonlien" href="http://www.eisti.fr">EISTI
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 </body>
 </html>

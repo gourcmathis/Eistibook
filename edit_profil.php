4804d62cbc014a759e3b28d2c1f10c477360f673
Roxane Chatry <roxane.chatry@eisti.fr>
2019-05-24 10:57:29 +0200

    Update edit_profil.php

--- edit_profil.php
+++ edit_profil.php
@@ -103,6 +103,7 @@ if ($acces<>"no") {
 ?>
 
 <div class='milieu'>
+  <form class='form-horizontal' role='form' action='valider_modif.php' method='POST'>
 	<!-- informations générales, non modifiables -->
 	<div>
 		<?php
@@ -111,17 +112,27 @@ if ($acces<>"no") {
 			<h3> ".$infos['NOM']." </h3>
     			<h4> ".$infos['PRENOM']."</h4>
     			<p>Né le ".$infos['NAISSANCE']."</p>";
-    		?>
+    		if (!empty($infos["SEXE"])) {
+    			echo "<p> Genre : ".$infos["SEXE"]."<p>";
+    		} else {
+    			echo "<p> Votre genre n'est pas défini. </p>";
+    		?>	
+    			<label><input type='radio' class='form-control' id='Homme' name='genre' />Homme</label>
+    			<label><input type='radio' class='form-control' id='Femme' name='genre' />Femme</label>
+    			<label><input type='radio' class='form-control' id='Autre' name='genre' />Autre</label> 
+    		<?php } ?>  	
     	</div>
+    	
+    	
+    	
     	
     	<!-- promotion -->
     	<div>
     		<h3>Votre promotion</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
+
 			<p>Vous avez redoublé ? Vous pouvez ici changer votre promotion. Séléctionnez votre nouvelle promotion : </p>
                             <input type="number" class="form-control" id="promo" name="promo" value="<?php echo $infos['PROMOTION']; ?>" required="required" >
-			<button type="submit" class="bouton">Valider</button>
-		</form>	
+
 	</div> 		
 		
 		
@@ -129,25 +140,22 @@ if ($acces<>"no") {
 	<div> 
 		<h3>Votre photo de profil</h3>
 		<?php
-		if (isset($infos['photo'])) {
-			$src=$infos['photo'];
+		if (isset($infos['PHOTO'])) {
+			$src=$infos['PHOTO'];
 			echo "<p><img class='photoprofil' src='".$src."'></img></p>";
 		} else {
 			echo "<p><img class='photoprofil' src='poulet.jpg'></img></p><p> Vous n'avez pas de photo de profil. </p>";
 		}
 		?>
 		<!-- modifier : -->
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<p>Vous souhaitez modifier votre photo de profil ? Ecrivez ici l'adresse de la photo que vous souhaitez utiliser : </p>
 			<input type="text" class="form-control" id="photo" name="photo" placeholder="poulet.jpg" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
+
 	</div>
 
 	<!-- citation -->
 	<div>
 		<h3>Votre citation</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['CITATION'])) {
 				echo "<p>Vous voulez modifier votre citation ? Entrez-la dans le champ suivant : </p> ";
@@ -158,15 +166,13 @@ if ($acces<>"no") {
 			} 
 			?>
 			<textarea id="citation" name="citation" required="required"><?php echo $cit; ?></textarea>
-			<button type="submit" class="bouton">Valider</button>
-		</form>
+
 	</div>
 
 
 	<!-- introduction -->
 	<div>
 		<h3>Votre introduction</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['INTRO'])) {
 				echo "<p>Vous voulez modifier votre introduction ? Entrez-la dans le champ suivant : </p> ";
@@ -177,15 +183,12 @@ if ($acces<>"no") {
 			} 
 			?>
 			<textarea id="intro" name="intro" required="required"><?php echo $intro; ?></textarea>
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
 
 	
 	<!-- profession -->
 	<div>
 		<h3>Votre profession</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['PROFESSION'])) {
 				echo "<p>Vous avez récemment changé de profession ? Mettez votre profil à jour : </p> ";
@@ -196,15 +199,12 @@ if ($acces<>"no") {
 			} 
 			?>
 			<input type="text" class="form-control" id="profession" name="profession" value="<?php echo $prof; ?>" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
 
 
 	<!-- emploi -->
 	<div>
 		<h3>Votre emploi</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['EMPLOI'])) {
 				echo "<p>Vous avez récemment changé d'emploi ? Mettez votre profil à jour : </p> ";
@@ -215,15 +215,12 @@ if ($acces<>"no") {
 			} 
 			?>
 			<input type="text" class="form-control" id="emploi" name="emploi" value="<?php echo $emploi; ?>" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
 
 	
 	<!-- ville -->
 	<div>
 		<h3>Votre Ville</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['VILLE'])) {
 				echo "<p>Vous venez de déménager ? Dites-le nous : </p> ";
@@ -234,15 +231,12 @@ if ($acces<>"no") {
 			} 
 			?>
 			<input type="text" class="form-control" id="ville" name="ville" value="<?php echo $ville; ?>" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
 	
 	
 	<!-- loisirs -->
 	<div>
 		<h3>Vos loisirs</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			if (!empty($infos['LOISIR'])) {
 				echo "<p>Un nouveau loisir ? Partagez-le :</p> ";
@@ -253,28 +247,103 @@ if ($acces<>"no") {
 			} 
 			?>
 			<input type="text" class="form-control" id="loisir" name="loisir" value="<?php echo $loisir; ?>" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
 	
 	
-	<!-- loisirs -->
+	<!-- caractère -->
 	<div>
 		<h3>Votre caractère</h3>
-		<form class='form-horizontal' role='form' action='edit_profil.php?perso=<?php echo $_SESSION['login']; ?>' method='POST'>
 			<?php
 			$caract=$tableau[1];
+			$optCaract=$options[0];
 			if (!empty($caract)) {
 				echo "<p>Vous voulez développer votre caractère ? </p> ";
+				
+
 			} else {
 				echo "<p>Vous n'avez rien indiqué sur votre caractère :( </p>";
-			} 
+			}
+			foreach ($optCaract as $opt) {
+				$valeur=$opt[0];
+				$id=$opt[1];
+				echo "<label><input type='checkbox' class='form-control' id='".$id."' name='caractere'";
+				if (in_array($valeur,$caract)) {
+					echo "checked";
+				}
+				echo "/>$valeur</label>";
+			}
 			?>
-			<input type="text" class="form-control" id="loisir" name="loisir" value="<?php echo $loisir; ?>" required="required">
-			<button type="submit" class="bouton">Valider</button>
-		</form>
 	</div>
-
+	
+	<!-- compétences -->
+	<div>
+		<h3>Vos compétences </h3>
+			<?php
+			$comp=$tableau[2];
+			$optComp=$options[1];
+			if (!empty($comp)) {
+				echo "<p>Vous voulez ajouter de nouvelles compétences ? </p> ";
+			} else {
+				echo "<p>Vous n'avez pas indiqué de compétences sur votre profil :( </p>";
+			}
+			foreach ($optComp as $opt) {
+				$valeur=$opt[0];
+				$id=$opt[1];
+				echo "<label><input type='checkbox' class='form-control' id='".$id."' name='competence'";
+				if (in_array($valeur,$comp)) {
+					echo "checked";
+				}
+				echo "/>$valeur</label>";
+			}
+			?>
+	</div>
+	
+	<!-- langues -->
+	<div>
+		<h3>Les langues que vous parlez</h3>
+			<?php
+			$langues=$tableau[3];
+			$optlangues=$options[2];
+			if (!empty($langues)) {
+				echo "<p>Vous avez appris une nouvelle langue ? </p> ";
+			} else {
+				echo "<p>Vous ne parlez aucune langue ? Indiquez au moins votre langue maternelle. </p>";
+			}
+			foreach ($optlangues as $opt) {
+				$valeur=$opt[0];
+				$id=$opt[1];
+				echo "<label><input type='checkbox' class='form-control' id='".$id."' name='langues'";
+				if (in_array($valeur,$langues)) {
+					echo "checked";
+				}
+				echo "/>$valeur</label>";
+			}
+			?>
+	</div>
+	
+	
+	<!-- outils -->
+	<div>
+		<h3>Les outils que vous maitrisez</h3>
+			<?php
+			$outils=$tableau[4];
+			$optOutils=$options[3];
+			if (!empty($outils)) {
+				echo "<p>Vous avez appris une nouvelle langue ? </p> ";
+			} else {
+				echo "<p>Vous ne parlez aucune langue ? Indiquez au moins votre langue maternelle. </p>";
+			}
+			foreach ($optOutils as $opt) {
+				$valeur=$opt[0];
+				$id=$opt[1];
+				echo "<label><input type='checkbox' class='form-control' id='".$id."' name='outils'";
+				if (in_array($valeur,$outils)) {
+					echo "checked";
+				}
+				echo "/>$valeur</label>";
+			}
+			?>
+	</div>
 
 
 
@@ -332,13 +401,14 @@ if ($acces<>"etranger") {
 
 } 
 ?>
+	<button type="submit" class="bouton">Valider mes modifications</button>
+  </form>
 </div>
 
 <!-- TODO 
-	- ajouter sexe en radio
 	- mettre en forme
-	- chekboxes pour caractère, langue, outils, compétences
-	- fonction chargerOptions() dans util.php pour récupérer les options de caractère, langue ... 
+	- fonction pour le bouton valider (autre page) : redirect vers le profil avec un messae  "vos modifications ont bien été enregistrées"
+
 
 -->
 

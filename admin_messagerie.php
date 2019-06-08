7087bcd355125abea6b7eb2823776b949f62e0b4
Guillaume Proton <guillaume.proton@eisti.fr>
2019-06-08 11:07:42 +0200

    ajout bouton onclick supprimer message

--- admin_messagerie.php
+++ admin_messagerie.php
@@ -4,14 +4,16 @@ include("admin_fonctions.php");
 $id_util=$_POST['id_util'];
 
 $tabMsg=chargeMessagerie($id_util);
-
 echo "<hr>";
 for ($i=0; $i < count($tabMsg); $i++) { 
-	echo "ID du message : ".$tabMsg[$i]['id']."<br>";
+	$idMsg=$tabMsg[$i]['id'];
+	echo "ID du message : ".$idMsg."<br>";
 	echo "<br>ID de l'expéditeur : ".$tabMsg[$i]['id_expediteur'];
 	echo "<br>Date du message : ".$tabMsg[$i]['date'];
 	echo "<br>Titre : ".$tabMsg[$i]['titre'];
-	echo "<br>Message : ".$tabMsg[$i]['message'];
+	echo "<br>Message : ".$tabMsg[$i]['message']."<br><br>";
+	echo "<input type='button' class='bouton_admin' value='Supprimer message' onclick='supprMsg($idMsg)'/>";
+	echo "<span id='del_msg'></span>";
 	echo "<hr>";
 }
 ?>
